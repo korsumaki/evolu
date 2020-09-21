df965b4465f863274490043e30dfdb6876107942
Markku Korsumäki <markku.korsumaki@gmail.com>
2020-09-21 23:03:38 +0300

    Field class updated

--- src/main/kotlin/evolu/Field.kt
+++ src/main/kotlin/evolu/Field.kt
@@ -1,7 +1,17 @@
 package evolu
 
 class Field(val width: Int, val height: Int) {
-    val numOfDiamonds = 7
+    private val numOfDiamonds = 7
+    var array = ArrayList<Spot>(width * height)
+
+    // Initialize field with Empty spots
+    init {
+        for (x in 0..width) {
+            for (y in 0..height) {
+                array.add(Spot.Empty)
+            }
+        }
+    }
 
     enum class Spot {
         Empty,
@@ -9,23 +19,48 @@ class Field(val width: Int, val height: Int) {
         Diamond
     }
 
-    var array: ByteArray = ByteArray(width * height)
-
     fun getSpot(x: Int, y: Int): Spot {
         if (x < 0 || x >= width) return Spot.Wall
         if (y < 0 || y >= height) return Spot.Wall
 
-        return Spot.Empty
+        return array[x + y*height]
     }
 
     fun setSpot(x: Int, y: Int, value: Spot) {
+        if (x < 0 || x >= width) return
+        if (y < 0 || y >= height) return
 
+        array[x + y * height] = value
     }
 
     fun randomize() {
-
+        var diamondsLeft = numOfDiamonds
+        while (diamondsLeft > 0) {
+            val x = (Math.random() * width).toInt()
+            val y = (Math.random() * height).toInt()
+            if (getSpot(x, y) == Spot.Empty) {
+                setSpot(x, y, Spot.Diamond)
+                diamondsLeft--
+            }
+        }
     }
-    fun visualize() {
 
+    private val emptyCharacter = '.'
+    private val wallCharacter = '#'
+    private val diamondCharacter = '$'
+
+    fun visualize() {
+        // Start from -1 to draw also walls
+        for (x in -1..width) {
+            for (y in -1..height) {
+                when (getSpot(x, y)) {
+                    Spot.Empty -> print(emptyCharacter)
+                    Spot.Wall -> print(wallCharacter)
+                    Spot.Diamond -> print(diamondCharacter)
+                }
+                print(' ') // Give more space between columns
+            }
+            println()
+        }
     }
 }
--- src/test/kotlin/evolu/FieldTest.kt
+++ src/test/kotlin/evolu/FieldTest.kt
@@ -1,6 +1,5 @@
 package evolu
 
-//import org.junit.jupiter.api.Assertions.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -10,10 +9,19 @@ internal class FieldTest {
     fun getSpot() {
         val field = Field(5, 5)
 
+        assertEquals(Field.Spot.Wall, field.getSpot(-1,0))
+        assertEquals(Field.Spot.Wall, field.getSpot(0,-1))
         assertEquals(Field.Spot.Empty, field.getSpot(0,0))
+        assertEquals(Field.Spot.Empty, field.getSpot(4,4))
+        assertEquals(Field.Spot.Wall, field.getSpot(5,4))
+        assertEquals(Field.Spot.Wall, field.getSpot(4,5))
     }
 
     @Test
     fun setSpot() {
+        val field = Field(5, 5)
+        assertEquals(Field.Spot.Empty, field.getSpot(2,2))
+        field.setSpot(2,2,Field.Spot.Diamond)
+        assertEquals(Field.Spot.Diamond, field.getSpot(2,2))
     }
 }
